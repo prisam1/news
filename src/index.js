fcbcae2e62a69cd94e61c272edccfe2e7d471c0e
Pritam <pritamsamaddar840@gmail.com>
2023-09-30 22:01:45 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
