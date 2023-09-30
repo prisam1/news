d9de46d76c8cc6d24c744aee0d13b003a34255ac
Pritam <pritamsamaddar840@gmail.com>
2023-09-30 22:22:04 +0530

    done

--- src/redux/newsAction.js
+++ src/redux/newsAction.js
@@ -1,20 +1,12 @@
 import axios from 'axios'
 
-const API_KEY = '83ff12a7085541c2908f9e00b8499e02' 
+const API_KEY = '83ff12a7085541c2908f9e00b8499e02'
 const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines'
 const COUNTRY = 'in'
 
 export const fetchNews = () => async (dispatch) => {
   try {
-    const headers = {
-      'User-Agent': 'YourUserAgent',
-      'Upgrade': 'HTTP/1.1',
-    };
-
-    const response = await axios.get(`${NEWS_API_URL}?country=${COUNTRY}&apiKey=${API_KEY}`, {
-      headers,
-    })
-
+    const response = await axios.get(`${NEWS_API_URL}?country=${COUNTRY}&apiKey=${API_KEY}`)
     dispatch({ type: 'FETCH_NEWS_SUCCESS', payload: response.data.articles })
   } catch (error) {
     dispatch({ type: 'FETCH_NEWS_FAILURE', payload: error.message })
