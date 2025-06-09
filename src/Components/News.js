2462bae11e7894848ee33c3e3ef00c4694e207e9
Pritam <pritamsamaddar840@gmail.com>
2025-06-09 20:52:26 +0530

    done

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,7 +4,7 @@ import { setFilters } from "../redux/slice/newsSlice";
 import { useNews } from "../hooks/useNews";
 import "../styles/news.css";
 
-export const News = () => {
+const News = () => {
   const dispatch = useDispatch();
   const { articles, filters } = useSelector((state) => state.news);
   const { loading, error } = useNews();
@@ -89,3 +89,5 @@ export const News = () => {
     </div>
   );
 };
+
+export default News
